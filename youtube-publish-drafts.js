f32949a01bd1f7c564034bccc4211d181f18ccb4
kubo25 <9075875+kubo25@users.noreply.github.com>
2021-12-15 15:19:36 +0100

    Add auto updates

--- youtube-publish-drafts.js
+++ youtube-publish-drafts.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name         Youtube publish drafts
 // @namespace    https://github.com/kubo25/youtube-publish-drafts
-// @version      0.1
+// @version      0.2
+// @updateURL    https://raw.githubusercontent.com/kubo25/youtube-publish-drafts/master/youtube-publish-drafts.js
+// @downloadURL  https://raw.githubusercontent.com/kubo25/youtube-publish-drafts/master/youtube-publish-drafts.js
 // @description  Publish all drafts
 // @author       Niedzwiedzw, kubo25
 // @icon         https://www.google.com/s2/favicons?domain=youtube.com
